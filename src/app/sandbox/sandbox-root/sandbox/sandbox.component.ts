c579ce0d18ed07a0389930b4e85084e1e86da611
yasmany1111 <yasmany1998@gmail.com>
2020-04-17 20:02:53 +0200

    Adding scheduler

--- src/app/sandbox/sandbox-root/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox-root/sandbox/sandbox.component.ts
@@ -1,77 +1,16 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { IAppState } from '../../../core/store-app/reducers';
-import { SandboxActivated } from '../../store-sandbox/actions/sandbox.actions';
-import { selectIsSandboxActive } from '../../store-sandbox/selectors/sandbox.selectors';
-import { take, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
-import { translate, TranslocoService } from '@ngneat/transloco';
+import { extend } from '@syncfusion/ej2-base';
+import { EventSettingsModel, DayService, WeekService, WorkWeekService, MonthService, AgendaService, ResizeService, DragAndDropService } from '@syncfusion/ej2-angular-schedule';
+import { scheduleData } from '../../mock-data';
 
 @Component({
   selector: 'app-sandbox',
   templateUrl: './sandbox.component.html',
-  styleUrls: ['./sandbox.component.scss']
+  styleUrls: ['./sandbox.component.scss'],
 })
-export class SandboxComponent implements OnInit, OnDestroy {
-  public isActivated = false;
-  public textToTranslate = 'I love programming';
-
-  private destroy$: Subject<void> = new Subject<void>();
-
-  constructor(private store: Store<IAppState>, private translocoService: TranslocoService) {}
-
-  public ngOnInit() {
-    this.store
-      .pipe(select(selectIsSandboxActive))
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((isActive: boolean) => console.log('Is Active from store: ', isActive));
-    if (true) {
-      const test = 0;
-    }
-
-    this.dynamicTranslation();
-  }
-
-  public testAction() {
-    this.isActivated = !this.isActivated;
-    this.store.dispatch(new SandboxActivated({ isActive: this.isActivated }));
-  }
-
-  public translateProgrammatically() {
-    // WAYS TO TRANSLATE
-
-    // this.translocoService.selectTranslateObject('DYNAMIC_TRANSLATE')
-    //   .pipe(take(1))
-    //   .subscribe({
-    //     next: (translationObject) => {
-    //       const values: string[] = Object.values(translationObject);
-    //       console.log(values);
-    //       // If there are many values, we should just push to array or map an array of objects
-    //       this.textToTranslate = values[0];
-    //     }
-    //   });
-    // this.textToTranslate = this.translocoService.translate('DYNAMIC_TRANSLATE.TEXT');
-    this.textToTranslate = translate('DYNAMIC_TRANSLATE.TEXT');
-  }
-
-  public ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  /**
-   * This translation changes with language change.
-   */
-  private dynamicTranslation() {
-    this.translocoService
-      .selectTranslate('DYNAMIC_TRANSLATE.TEXT')
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (translation) => {
-          console.log(translation);
-          // If there are many values, we should just push to array or map an array of objects
-          this.textToTranslate = translation;
-        }
-      });
-  }
+export class SandboxComponent {
+  public selectedDate: Date = new Date(2019, 0, 10);
+  public eventSettings: EventSettingsModel = {
+    dataSource: <any[]>extend([], scheduleData, null, true),
+  };
 }
--- src/app/sandbox/sandbox.module.ts
+++ src/app/sandbox/sandbox.module.ts
@@ -1,3 +1,17 @@
+import { TreeViewModule } from '@syncfusion/ej2-angular-navigations';
+import { DropDownListAllModule, MultiSelectAllModule } from '@syncfusion/ej2-angular-dropdowns';
+import { MaskedTextBoxModule, UploaderAllModule } from '@syncfusion/ej2-angular-inputs';
+import { ToolbarAllModule, ContextMenuAllModule } from '@syncfusion/ej2-angular-navigations';
+import { ButtonAllModule } from '@syncfusion/ej2-angular-buttons';
+import { CheckBoxAllModule } from '@syncfusion/ej2-angular-buttons';
+import {
+  DatePickerAllModule,
+  TimePickerAllModule,
+  DateTimePickerAllModule,
+} from '@syncfusion/ej2-angular-calendars';
+import { NumericTextBoxAllModule } from '@syncfusion/ej2-angular-inputs';
+import { ScheduleAllModule, RecurrenceEditorAllModule } from '@syncfusion/ej2-angular-schedule';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { SandboxRoutingModule } from './sandbox-routing.module';
@@ -17,7 +31,23 @@ import * as fromSandbox from './store-sandbox/reducers/sandbox.reducer';
     SandboxRoutingModule,
     MatToolbarModule,
     StoreModule.forFeature('sandboxState', fromSandbox.sandboxReducer),
-    TranslocoLanguageModule
-  ]
+    TranslocoLanguageModule,
+    HttpModule,
+    ScheduleAllModule,
+    RecurrenceEditorAllModule,
+    NumericTextBoxAllModule,
+    DatePickerAllModule,
+    TimePickerAllModule,
+    DateTimePickerAllModule,
+    CheckBoxAllModule,
+    ToolbarAllModule,
+    DropDownListAllModule,
+    ContextMenuAllModule,
+    MaskedTextBoxModule,
+    UploaderAllModule,
+    MultiSelectAllModule,
+    TreeViewModule,
+    ButtonAllModule,
+  ],
 })
 export class SandboxModule {}
